8f29a9e2ed7aaa279185e9044b240afbebcfda43
LeaKoh <20210808@duksung.ac.kr>
2022-07-28 18:18:15 +0900

    지도& 돌아가기 버튼 추가

--- app/src/main/java/com/example/smokingarealist/HomeFragment.java
+++ app/src/main/java/com/example/smokingarealist/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
@@ -16,15 +17,51 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback{
 
     String strTitle[] = {"서울역 공항철도 15번 출구","용산역 1,2번출구","용산구청 앞","서울 용산 경찰서","용산경찰서 원효지구대","서울지방보훈청","용산세무서","국방부","순천향대학병원","하얏트호텔","용산 아이파크몰","동아사이언스 앞", "용산 아이피아"};
     String strDescription[]= {"서울특별시 용산구 청파동 297","서울특별시 용산구 한강대로 42","서울특별시 용산구 이태원동 녹사평대로 150","서울특별시 용산구 원효로89길 24","서울특별시 용산구 백범로 329","서울특별시 용산구 한강로동 131","서울특별시 용산구 서빙고로24길 15","서울특별시 용산구 용산동3가","서울특별시 용산구 한남동 657-76","서울특별시 용산구 한남동 747-7","서울특별시 용산구 한강로3가 한강대로23길 55 용산역 3","서울특별시 용산구 한강로3가 3-23","서울특별시 용산구 한강로2가 2-185"};
     int image = R.drawable.img_1;
     Context ct;
 
+    private Button btn;
+    private MapView mapView;
+    private Button backbtn;
+
+    public HomeFragment(){}
+
+    public static HomeFragment newInstance(){
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
@@ -35,7 +72,48 @@ public class HomeFragment extends Fragment {
         MyAdapter adapter = new MyAdapter(ct, strTitle, strDescription, image);
         listView.setAdapter(adapter);
 
+        listView.setVisibility(View.INVISIBLE);
+
+        mapView = (MapView)v.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+
+        btn = (Button) v.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapView.setVisibility(View.INVISIBLE);
+                listView.setVisibility(View.VISIBLE);
+                btn.setVisibility(View.INVISIBLE);
+                backbtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        backbtn = (Button) v.findViewById(R.id.backbtn);
+        backbtn.setVisibility(View.INVISIBLE);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapView.setVisibility(View.VISIBLE);
+                listView.setVisibility(View.INVISIBLE);
+                btn.setVisibility(View.VISIBLE);
+                backbtn.setVisibility(View.INVISIBLE);
+            }
+        });
+
         return v;
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng SEOUL = new LatLng(37.553149, 126.968881);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
     }
 }
 
@@ -69,6 +147,8 @@ class MyAdapter extends ArrayAdapter<String> {
 
         return listview_item;
     }
+
+
 
 }
 
--- app/src/main/java/com/example/smokingarealist/MainActivity.java
+++ app/src/main/java/com/example/smokingarealist/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
