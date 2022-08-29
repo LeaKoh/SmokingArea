ade0bf82a60f85a2ad140566721366b497b4e76d
joococo <2000violet@naver.com>
2022-08-29 22:17:37 +0900

    search

--- app/src/main/java/com/example/smokingarealist/HomeFragment.java
+++ app/src/main/java/com/example/smokingarealist/HomeFragment.java
@@ -1,20 +1,30 @@
 package com.example.smokingarealist;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,9 +33,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment implements OnMapReadyCallback{
-
-
 
     String strTitle[] = {"서울역 공항철도 15번 출구.국립극단","서울역 공항철도 15번 출구","용산역 광장앞(용산아이파크몰)","용산구청 앞","서울 용산 경찰서","용산경찰서 원효지구대","서울지방보훈청","용산세무서","국방부","순천향대학병원",
             "하얏트호텔","용산 아이파크몰","동아사이언스 앞", "용산 아이피아","용산 전자랜드","아모레퍼시픽","제일기획","제이엘레지던스","충영빌딩","e테크밸리",
@@ -34,6 +45,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
             "한남빌딩","용산역 비비안", "서울역 풍산아이원플러스","용산 세광음악사","한화빌딩","보은개발","애전빌딩","갈월동 오피스","KCC IT빌딩","금강토탈패션할인매장",
             "용산빌딩","청룡빌딩","롯데지알에스","서경산업 금강빌딩","수빌딩","우리빌딩","기업은행 용산지점","진흥기업 진흥빌딩","용암빌딩 한치과","하나은행 LS용산타워지점",
             "한국폴리텍대학 서울정수캠퍼스 운동장", "한국폴리텍1대학 서울정수캠퍼스","한국폴리텍대학 서울정수캠퍼스 제2공학관","한국폴리텍대학 서울정수캠퍼스 학생회관"};
+
     String strDescription[]= {"서울특별시 용산구 청파동 297","서울특별시 용산구 동자동 43-227","서울특별시 용산구 한강대로 42","서울특별시 용산구 이태원동 녹사평대로 150","서울특별시 용산구 원효로89길 24","서울특별시 용산구 백범로 329","서울특별시 용산구 한강로1가 107","서울특별시 용산구 서빙고로24길 15","서울특별시 용산구 용산동3가","서울특별시 용산구 한남동 657-76",
             "서울특별시 용산구 한남동 747-7","서울특별시 용산구 한강로3가 한강대로23길 55 용산역 3","서울특별시 용산구 한강로3가 3-23","서울특별시 용산구 한강로2가 2-185","서울특별시 용산구 한강로3가 16-9","서울특별시 용산구 한강로2가 424","서울특별시 용산구 한남동 738-29", "서울특별시 용산구 용문동 10-4","서울특별시 용산구 용문동 28-5","서울특별시 용산구 원효로3가 51-37",
             "서울특별시 용산구 원효로3가 43-39","서울특별시 용산구 원효로3가 124-1","서울특별시 용산구 원효로4가 113-71","서울특별시 용산구 신계동 7-3","서울특별시 용산구 원효로1가 57-11","서울특별시 용산구 원효로2가 37-3", "서울특별시 용산구 원효로2가 92-5","서울특별시 용산구 문배동 30-10","서울특별시 용산구 신계동 40-1","서울특별시 용산구 한강로2가 101",
@@ -41,6 +53,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
             "서울특별시 용산구 한남동 737-37","서울특별시 용산구 한강로3가 40-999","서울특별시 용산구 서계동 219-17","서울특별시 용산구 서계동 232-32", "서울특별시 용산구 서계동 53-24","서울특별시 용산구 서계동 130-2","서울특별시 용산구 청파동3가 80-7","서울특별시 용산구 갈월동 103-17","서울특별시 용산구 갈월동 7-50","서울특별시 용산구 갈월동 88-18",
             "서울특별시 용산구 갈월동 92","서울특별시 용산구 갈월동 98-38","서울특별시 용산구 갈월동 98-6","서울특별시 용산구 남영동 10-1", "서울특별시 용산구 한강로2가 2-177","서울특별시 용산구 남영동 129-4","서울특별시 용산구 남영동 87","서울특별시 용산구 후암동 105-64","서울특별시 용산구 후암동 175-9","서울특별시 용산구 한강로2가 191-3",
             "서울특별시 용산구 보광동 238-2","서울특별시 용산구 보광동 238-2","서울특별시 용산구 보광동 238-2","서울특별시 용산구 보광동 238-2"};
+
     double latitude[] = {37.553149,37.55376,37.528404,37.532709,37.541169,37.538716,37.534923,37.523302,37.533237,37.534466,
             37.539372,37.529492,37.533068,37.533417,37.532818,37.52911,37.535103,37.536678,37.538329,37.533234,
             37.534626,37.533918,37.531999,37.53461,37.538839,37.536223,37.534709,37.535767,37.536202,37.534231,
@@ -48,6 +61,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
             37.535022,37.528894,37.55492,37.553965,37.552032,37.551185,37.54175,37.540529,37.548507,37.543561,
             37.542611,37.541424,37.54154,37.544739,37.532824,37.54096,37.541995,37.5485,37.54589, 37.528002,
             37.529136,37.52871,37.529323,37.529799};
+
     double longitude[] = {126.968881,126.969662,126.965569,126.99,126.96765,126.96589,126.974192, 126.96868,126.97859,127.004585,
             126.997264,126.964318,126.963521,126.96736,126.958895,126.968532,126.998616,126.961147,126.959979,126.957656,
             126.959002,126.95763,126.953751,126.966177,126.968402,126.963549,126.960734, 126.969268,126.961404,126.970651,
@@ -56,13 +70,16 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
             126.972137,126.972679,126.971791, 126.973179,126.970666,126.973516,126.973179,126.977574,126.976959,126.967632,
             126.996483,126.996226,126.995976,126.996498};
 
+    String strTitle2[] = {};
+    String strDescription2[] = {};
+
     int image = R.drawable.location;
-    Context ct;
+
+    private Context ct;
 
    MapsFragment mapsFragment = new MapsFragment();
 
-    private Button btn;
-    private Button backbtn;
+    private ImageButton backbtn;
 
     public HomeFragment(){}
 
@@ -70,6 +87,16 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         HomeFragment fragment = new HomeFragment();
         return fragment;
     }
+
+    private EditText search_et;
+    private ImageButton searchBtn;
+    private String str;
+    public ListView listView;
+    public ListView listView2;
+    public MyAdapter adapter2;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -89,17 +116,80 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         super.onResume();
     }
 
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+
         ct = container.getContext();
 
-        ListView listView = (ListView)v.findViewById(R.id.listview);
+
+        listView = (ListView)v.findViewById(R.id.listview);
+        listView2 = (ListView)v.findViewById(R.id. listview2);
+
+        Button button = (Button) v.findViewById(R.id. button);
+
+        adapter2 = new MyAdapter(ct, strTitle2, strDescription2, image);
+        listView2.setAdapter(adapter2);
+
+
         MyAdapter adapter = new MyAdapter(ct, strTitle, strDescription, image);
+        adapter2 = new MyAdapter(ct, strTitle2, strTitle2, image);
+
+
         listView.setAdapter(adapter);
 
+
+        search_et = (EditText) v.findViewById(R.id.editText);
+        searchBtn = (ImageButton) v.findViewById(R.id. buttonSearch);
+        backbtn = (ImageButton) v.findViewById(R.id.backbtn);
+
+        backbtn.setVisibility(View.INVISIBLE);
         listView.setVisibility(View.INVISIBLE);
+        listView2.setVisibility(View. INVISIBLE);
+
+        search_et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = search_et.getText().toString();
+                search(text);
+            }
+
+
+            private void search(String charText) {
+
+                adapter2.clear();
+
+                if (charText.length() == 0) {
+                    adapter2.addAll();
+                }
+                else
+                {
+                    for (int i = 0; i < strTitle.length; i++)
+                        if (strTitle[i].toLowerCase().contains(charText)) {
+                            int n = 0;
+                            strTitle2[n] = strTitle[i];
+                            strDescription2[n] = strDescription2[i];
+                            n++;
+                        }
+                }
+
+                adapter2.notifyDataSetChanged();
+            }
+        });
 
         SupportMapFragment supportMapFragment=(SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.map);
@@ -107,25 +197,38 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
 //        supportMapFragment.getMapAsync(this);
 
 
-        btn = (Button) v.findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 supportMapFragment.getView().setVisibility(View.INVISIBLE);
-                listView.setVisibility(View.VISIBLE);
-                btn.setVisibility(View.INVISIBLE);
+                //listView2.setVisibility(View.VISIBLE);
+                listView.setVisibility(View. VISIBLE);
+                searchBtn.setVisibility(View.INVISIBLE);
                 backbtn.setVisibility(View.VISIBLE);
+                button.setVisibility(View.INVISIBLE);
             }
         });
 
-        backbtn = (Button) v.findViewById(R.id.backbtn);
-        backbtn.setVisibility(View.INVISIBLE);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                supportMapFragment.getView().setVisibility(View.INVISIBLE);
+                //listView2.setVisibility(View.VISIBLE);
+                searchBtn.setVisibility(View.INVISIBLE);
+                backbtn.setVisibility(View.VISIBLE);
+                button.setVisibility(View.INVISIBLE);
+            }
+        });
+
         backbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 supportMapFragment.getView().setVisibility(View.VISIBLE);
+                listView2.setVisibility(View.INVISIBLE);
                 listView.setVisibility(View.INVISIBLE);
-                btn.setVisibility(View.VISIBLE);
+                searchBtn.setVisibility(View.VISIBLE);
                 backbtn.setVisibility(View.INVISIBLE);
             }
         });
@@ -144,6 +247,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
 
     }
 
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         LatLng SEOUL = new LatLng(37.553149,126.968881 );
@@ -159,7 +263,9 @@ class MyAdapter extends ArrayAdapter<String> {
     Context context;
     String rTitle[];
     String rDescription[];
+    
     int rImgs;
+
 
     MyAdapter(Context con, String sTitle[], String sDescription[], int imgs){
         super(con, R.layout.listview_item, R.id.name, sTitle);
@@ -167,6 +273,25 @@ class MyAdapter extends ArrayAdapter<String> {
         this.rTitle = sTitle;
         this.rDescription = sDescription;
         this.rImgs = imgs;
+    }
+
+
+    @NonNull
+    @Override
+    public Context getContext() {
+        return context;
+    }
+
+    public String getrTitle(int i) {
+        return rTitle[i];
+    }
+
+    public String[] getrDescription() {
+        return rDescription;
+    }
+
+    public int getrImgs() {
+        return rImgs;
     }
 
     @NonNull
@@ -191,8 +316,7 @@ class MyAdapter extends ArrayAdapter<String> {
     public static HomeFragment newInstance(){
         return new HomeFragment();
     }
-
-
-
 }
+
+
 
--- app/src/main/java/com/example/smokingarealist/MainActivity.java
+++ app/src/main/java/com/example/smokingarealist/MainActivity.java
@@ -11,15 +11,20 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
     BottomNavigationView bottomNavigationView;
+
+
 
     HomeFragment homeFragment = HomeFragment.newInstance();
     ChartFragment chartFragment = new ChartFragment();
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         bottomNavigationView = findViewById(R.id.bottom_bar);
 
